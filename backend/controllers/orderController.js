a536937797783f67a91c30bea52fb2ead6d52ec9
dnkc <dnkcdesign@gmail.com>
2021-03-02 10:15:07 -0500

    updated import error

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,5 +1,5 @@
 import asyncHandler from "express-async-handler";
-import Order from "../models/OrderModel.js";
+import Order from "../models/orderModel.js";
 
 // @ Create a new order
 // @ POST /api/orders
